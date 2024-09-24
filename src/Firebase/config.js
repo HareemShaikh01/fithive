d6f0878bde5b83da4e828b1e65c7ff36a4727986
HareemShaikh01 <hareemshaikh2006@gmail.com>
2024-09-24 19:13:52 +0500

    final commit

--- src/Firebase/config.js
+++ src/Firebase/config.js
@@ -1,14 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAU34g8OcOgVfY2d-ObE7uDUp59Zxu7POA",
-  authDomain: "fithive-436e7.firebaseapp.com",
-  projectId: "fithive-436e7",
-  storageBucket: "fithive-436e7.appspot.com",
-  messagingSenderId: "120206514999",
-  appId: "1:120206514999:web:dcc8f80abfd3e5f34e58b2",
-  measurementId: "G-TJ58Z02K3T"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
